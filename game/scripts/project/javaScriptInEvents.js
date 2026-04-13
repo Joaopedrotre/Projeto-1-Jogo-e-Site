5885b34b2f3d2af0d2b632250b8e1c5415f251f6
João Trevisan <joao.trevisan@grupointegrado.br>
2026-04-13 00:23:45 -0300

    Correção de arquivos e código HTML

---
+++ game/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,39 @@
+
+
+const scriptsInEvents = {
+
+	async EventSheetMain_Event8_Act4(runtime, localVars)
+	{
+		const player = runtime.objects.Player.getFirstInstance();
+		if (!player) return;
+		
+		if (player.instVars.Invincible) return;
+		
+		let lives = player.instVars.Lives;
+		if (lives <= 0) return;
+		
+		player.instVars.Lives = lives - 1;
+		lives = player.instVars.Lives;
+		
+		if (lives === 2) {
+		    const heart3 = runtime.objects.Heart3.getFirstInstance();
+		    if (heart3) heart3.destroy();
+		}
+		else if (lives === 1) {
+		    const heart2 = runtime.objects.Heart2.getFirstInstance();
+		    if (heart2) heart2.destroy();
+		}
+		else if (lives === 0) {
+		    const heart1 = runtime.objects.Heart.getFirstInstance();
+		    if (heart1) heart1.destroy();
+		    console.log("Game Over");
+		}
+		
+		player.instVars.Invincible = true;
+		
+		const bullet = runtime.objects.EnemyBullet.getFirstPickedInstance();
+		if (bullet) bullet.destroy();
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
