ac3842653da86c924c1e281d6faed9ec87964084
João Trevisan <joao.trevisan@grupointegrado.br>
2026-04-15 03:14:35 -0300

    Testes de inicialização do jogo

--- docs/game/scripts/project/javaScriptInEvents.js
+++ docs/game/scripts/project/javaScriptInEvents.js
@@ -2,7 +2,7 @@
 
 const scriptsInEvents = {
 
-	async EventSheetMain_Event14_Act3(runtime, localVars)
+	async EventSheetMain_Event36_Act1(runtime, localVars)
 	{
 		const player = runtime.objects.Player.getFirstInstance();
 		if (!player) return;
@@ -35,7 +35,40 @@ const scriptsInEvents = {
 		if (bullet) bullet.destroy();
 	},
 
-	async EventSheetMain_Event19_Act2(runtime, localVars)
+	async EventSheetMain_Event38_Act1(runtime, localVars)
+	{
+		const player = runtime.objects.Player.getFirstInstance();
+		if (!player) return;
+		
+		if (player.instVars.Invincible) return;
+		
+		let lives = player.instVars.Lives;
+		if (lives <= 0) return;
+		
+		player.instVars.Lives = lives - 1;
+		lives = player.instVars.Lives;
+		
+		if (lives === 2) {
+		    const heart3 = runtime.objects.Heart3.getFirstInstance();
+		    if (heart3) heart3.destroy();
+		}
+		else if (lives === 1) {
+		    const heart2 = runtime.objects.Heart2.getFirstInstance();
+		    if (heart2) heart2.destroy();
+		}
+		else if (lives === 0) {
+		    const heart1 = runtime.objects.Heart.getFirstInstance();
+		    if (heart1) heart1.destroy();
+		    console.log("Game Over");
+		}
+		
+		player.instVars.Invincible = true;
+		
+		const bullet = runtime.objects.DroneBullet.getFirstPickedInstance();
+		if (bullet) bullet.destroy();
+	},
+
+	async EventSheetMain_Event14_Act2(runtime, localVars)
 	{
 		const player = runtime.objects.Player.getFirstInstance();
 		if (!player) return;
